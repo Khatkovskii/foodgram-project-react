fa26c06b4b6464838acc9f7d47a40f92ff3eef5a
Gleb Khatkovskii <tosteg.gfmgf@gmail.com>
2023-07-31 19:47:20 +0400

    Create model Recipe

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import User
+
 
 class Ingredient(models.Model):
     name = models.CharField(
@@ -19,4 +21,45 @@ class Ingredient(models.Model):
         verbose_name_plural = 'Ингридиенты'
     
     def __str__(self) -> str:
-        return f'{self.name}, {self.unit}' 
+        return f'{self.name}, {self.unit}'
+
+
+class Recipe(models.Model):
+    name = models.CharField(
+        max_length=255,
+        db_index=True,
+        verbose_name='Рецепт'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Автор'
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        related_name='recipes',
+        verbose_name='Список ингридиентов',
+    )
+    image = models.ImageField(
+        blank=True,
+        upload_to='recipes/images',
+        verbose_name='Фото'
+    )
+    description = models.TextField(
+        verbose_name='Описание'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации'
+    )
+    
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+
+    def __str__(self) -> str:
+        return f'{self.name} автор {self.author.username}'
+        
